3e358fb0fc15b6893e9461e30a4ece194e0db516
Gautier Cuche <gautier.cuche@unil.ch>
2025-09-29 15:20:35 +0000

    done partially

--- problem3.py
+++ problem3.py
@@ -14,15 +14,18 @@ def get_numbers_from_user():
     numbers = []
 
     while True:
-        # TODO: Get input from user
-        # TODO: Check if user typed 'done'
-        # TODO: Try to convert to float and add to list
-        # TODO: Handle invalid input gracefully
-        pass
+        user_input = input("Enter a number or 'done' to finish: ").strip()
+        if user_input.lower() == 'done':
+            break
+        try:
+            num = float(user_input)
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input. Please enter a number or 'done'.")
 
     return numbers
 
-
+    
 def analyze_numbers(numbers):
     """
     Analyze the list and return a dictionary with:
